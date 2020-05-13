c7aeb54503d3476453cbc57af65b5d7966ebb52a
rezk0012 <alfred.rezk@gmail.com>
2020-05-13 13:20:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ mongoose.connect('mongodb+srv://User:Password01@cluster0-83jxn.mongodb.net/final
   console.log(error);
  })
 
-app.use('/images', express.static(path.join('images')))
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 
 app.use(express.json());
